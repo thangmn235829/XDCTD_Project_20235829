f28512625a7d9ad70dc4a30b54cb278c572f93ac
Thang Ma <thang.mn235829@sis.hust.edu.vn>
2025-11-24 16:14:44 +0700

    Fix keyword comparison in keywordEq function

--- Bai2/incompleted/token.c
+++ Bai2/incompleted/token.c
@@ -36,7 +36,7 @@ struct {
 
 int keywordEq(char *kw, char *string) {
   while ((*kw != '\0') && (*string != '\0')) {
-    if (*kw != toupper(*string)) break;
+    if (*kw != *string) break;
     kw ++; string ++;
   }
   return ((*kw == '\0') && (*string == '\0'));
