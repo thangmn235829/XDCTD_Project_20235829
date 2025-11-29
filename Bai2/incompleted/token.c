efaa45871dd403491469cb997dcbc3446c418dcd
Thang Ma <thang.mn235829@sis.hust.edu.vn>
2025-11-29 09:31:11 +0700

    Add new keywords and update printToken function
    Added new keywords 'REPEAT', 'UNTIL', 'BYTE', and 'STRING' to the token structure and updated the printToken function to handle these new keywords.

--- Bai2/incompleted/token.c
+++ Bai2/incompleted/token.c
@@ -1,13 +1,15 @@
-/* 
- * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+/* * @copyright (c) 2008, Hedspi, Hanoi University of Technology
  * @author Huu-Duc Nguyen
  * @version 1.0
  */
 
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h> 
+#include <stdio.h> // Cần cho printf
 #include "token.h"
 
+// Số lượng từ khóa được cập nhật lên 24
 struct {
   char string[MAX_IDENT_LEN + 1];
   TokenType tokenType;
@@ -31,12 +33,17 @@ struct {
   {"WHILE", KW_WHILE},
   {"DO", KW_DO},
   {"FOR", KW_FOR},
-  {"TO", KW_TO}
+  {"TO", KW_TO},
+  {"REPEAT", KW_REPEAT},
+  {"UNTIL", KW_UNTIL},
+  // Thêm hai từ khóa mới
+  {"BYTE", KW_BYTE},
+  {"STRING", KW_STRING}
 };
 
 int keywordEq(char *kw, char *string) {
   while ((*kw != '\0') && (*string != '\0')) {
-    if (*kw != *string) break; // Sá»­a: So sÃ¡nh cÃ³ phÃ¢n biá»‡t chá»¯ hoa/thÆ°á»ng
+    if (*kw != *string) break; 
     kw ++; string ++;
   }
   return ((*kw == '\0') && (*string == '\0'));
@@ -47,7 +54,7 @@ TokenType checkKeyword(char *string) {
   for (i = 0; i < KEYWORDS_COUNT; i++)
     if (keywordEq(keywords[i].string, string)) 
       return keywords[i].tokenType;
-  return TK_NONE;
+  return TK_IDENT;
 }
 
 Token* makeToken(TokenType tokenType, int lineNo, int colNo) {
@@ -55,5 +62,67 @@ Token* makeToken(TokenType tokenType, int lineNo, int colNo) {
   token->tokenType = tokenType;
   token->lineNo = lineNo;
   token->colNo = colNo;
+  // Khởi tạo string để tránh lỗi
+  memset(token->string, 0, MAX_STRING_LENGTH + 1); 
   return token;
+}
+
+void printToken(Token *token) {
+
+  switch (token->tokenType) {
+  case TK_NONE: printf("TK_NONE\n"); break;
+  case TK_IDENT: printf("TK_IDENT(%s)\n", token->string); break;
+  case TK_NUMBER: printf("TK_NUMBER(%d)\n", token->value); break;
+  case TK_CHAR: printf("TK_CHAR(\'%s\')\n", token->string); break;
+  case TK_STRING: printf("TK_STRING(\"%s\")\n", token->string); break; // In hằng xâu
+  case TK_EOF: printf("TK_EOF\n"); break;
+
+  case KW_PROGRAM: printf("KW_PROGRAM\n"); break;
+  case KW_CONST: printf("KW_CONST\n"); break;
+  case KW_TYPE: printf("KW_TYPE\n"); break;
+  case KW_VAR: printf("KW_VAR\n"); break;
+  case KW_INTEGER: printf("KW_INTEGER\n"); break;
+  case KW_CHAR: printf("KW_CHAR\n"); break;
+  case KW_ARRAY: printf("KW_ARRAY\n"); break;
+  case KW_OF: printf("KW_OF\n"); break;
+  case KW_FUNCTION: printf("KW_FUNCTION\n"); break;
+  case KW_PROCEDURE: printf("KW_PROCEDURE\n"); break;
+  case KW_BEGIN: printf("KW_BEGIN\n"); break;
+  case KW_END: printf("KW_END\n"); break;
+  case KW_CALL: printf("KW_CALL\n"); break;
+  case KW_IF: printf("KW_IF\n"); break;
+  case KW_THEN: printf("KW_THEN\n"); break;
+  case KW_ELSE: printf("KW_ELSE\n"); break;
+  case KW_WHILE: printf("KW_WHILE\n"); break;
+  case KW_DO: printf("KW_DO\n"); break;
+  case KW_FOR: printf("KW_FOR\n"); break;
+  case KW_TO: printf("KW_TO\n"); break;
+  case KW_REPEAT: printf("KW_REPEAT\n"); break;
+  case KW_UNTIL: printf("KW_UNTIL\n"); break;
+  case KW_BYTE: printf("KW_BYTE\n"); break; // In keyword BYTE
+  case KW_STRING: printf("KW_STRING\n"); break; // In keyword STRING
+
+  case SB_SEMICOLON: printf("SB_SEMICOLON\n"); break;
+  case SB_COLON: printf("SB_COLON\n"); break;
+  case SB_PERIOD: printf("SB_PERIOD\n"); break;
+  case SB_COMMA: printf("SB_COMMA\n"); break;
+  case SB_ASSIGN: printf("SB_ASSIGN\n"); break;
+  case SB_EQ: printf("SB_EQ\n"); break;
+  case SB_NEQ: printf("SB_NEQ\n"); break;
+  case SB_LT: printf("SB_LT\n"); break;
+  case SB_LE: printf("SB_LE\n"); break;
+  case SB_GT: printf("SB_GT\n"); break;
+  case SB_GE: printf("SB_GE\n"); break;
+  case SB_PLUS: printf("SB_PLUS\n"); break;
+  case SB_MINUS: printf("SB_MINUS\n"); break;
+  case SB_TIMES: printf("SB_TIMES\n"); break;
+  case SB_SLASH: printf("SB_SLASH\n"); break;
+  case SB_LPAR: printf("SB_LPAR\n"); break;
+  case SB_RPAR: printf("SB_RPAR\n"); break;
+  case SB_LSEL: printf("SB_LSEL\n"); break;
+  case SB_RSEL: printf("SB_RSEL\n"); break;
+  
+  case SB_MOD: printf("SB_MOD\n"); break;
+  case SB_EXPONENT: printf("SB_EXPONENT\n"); break;
+  }
 }
